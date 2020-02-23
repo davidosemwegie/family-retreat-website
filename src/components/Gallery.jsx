7987e1da2434c88a2af4b4c4e393a622825a96b5
David Osemwegie <david@Davids-MacBook-Pro.local>
2020-02-23 09:37:38 -0700

    made the other changes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,19 @@ import gallery from '../assets/gallery.png'
 
 import one from '../assets/1.jpg'
 import two from '../assets/2.jpg'
-import three from '../assets/2.jpg'
+import three from '../assets/3.jpg'
+import four from '../assets/4.jpg'
+import five from '../assets/5.jpg'
+import six from '../assets/6.jpg'
+import seven from '../assets/7.jpg'
+import eight from '../assets/8.jpg'
+import nine from '../assets/9.jpg'
+import ten from '../assets/10.jpg'
+import eleven from '../assets/11.jpg'
+import twelve from '../assets/12.jpg'
+import thirteen from '../assets/13.jpg'
+import fourteen from '../assets/14.jpg'
+import fifteen from '../assets/2.jpg'
 // import "react-responsive-carousel/lib/styles/carousel.min.css";
 // import { Carousel } from 'react-responsive-carousel';
 
@@ -47,8 +59,93 @@ class Gallery extends Component {
                                 alt="Third slide"
                             />
                         </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={four}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={five}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={six}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={seven}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={eight}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={nine}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={ten}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={eleven}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={twelve}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={thirteen}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={fourteen}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={fifteen}
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
                     </Carousel>
-                    <a target="_blank" href="https://www.showpass.com/youth-family-retreat/" className="info-box-text" id="ticket-box">Get Tickets</a>
+
+                    <a target="_blank" href="https://www.showpass.com/youth-family-retreat/" className="info-box-text" id="ticket-box-gallery">Get Tickets</a>
                 </div>
                 
             </div>
