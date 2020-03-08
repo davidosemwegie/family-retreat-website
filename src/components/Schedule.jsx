c4b831003e7a326246e0e8d807480443fbf95cbe
David Osemwegie <david@Davids-MBP.hitronhub.home>
2020-03-08 16:52:58 -0600

    updated schedule

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -16,7 +16,9 @@ class Schedule extends Component {
                                 <SDAY>THUR, MAR 19</SDAY>
                                 <SE time="7:00PM" title="Arrival" />
                                 <SE time="8:15PM" title="Dinner" />
-                                <SE time="9:30PM" title="Opening Rally" />
+                                <SE time="9:00PM" title="Pre-Service Prayer" />
+                                <SE time="9:30PM" title="Weekend Opener" />
+                                <SE time="10:30PM" title="Games Night" />
                                 <SE time="12:00AM" title="Lights Out" />
                             </ul>
                         </li>
@@ -24,13 +26,18 @@ class Schedule extends Component {
                             <ul>
                                 <SDAY>FRI, MAR 20</SDAY>
                                 <SE time="8:30AM" title="Breakfast" />
-                                <SE time="10:00AM" title="Bible Study" />
-                                <SE time="10:45AM" title="Activity" />
+                                <SE time="9:30AM" title="Free Time" />
+                                <SE time="10:00AM" title="Pre-Serice Activity" />
+                                <SE time="10:10AM" title="Bible Study / Special Guest" />
+                                <SE time="11:00AM" title="Outdoor Activities" />
                                 <SE time="12:30PM" title="Lunch" />
-                                <SE time="1:45PM" title="Session" />
-                                <SE time="3:00PM" title="Activity" />
+                                <SE time="1:45PM" title="Girls + Boys Session" />
+                                <SE time="3:00PM" title="Mission Impossible / Scavenger Hunt" />
+                                <SE time="4:30PM" title="Free Time" />
                                 <SE time="6:00PM" title="Dinner" />
+                                <SE time="7:00M" title="Praise and Worship" />
                                 <SE time="7:10PM" title="Session" />
+                                <SE time="8:00PM" title="Flex Time" />
                                 <SE time="8:40PM" title="Games Night" />
                                 <SE time="10:00PM" title="Campfire" />
                                 <SE time="11:20PM" title="Lights Out" />
@@ -40,21 +47,30 @@ class Schedule extends Component {
                             <ul>
                                 <SDAY>SAT, MAR 21</SDAY>
                                 <SE time="8:30AM" title="Breakfast" />
-                                <SE time="11:00AM" title="Breakout Session" />
-                                <SE time="1:00PM" title="Lunch" />
-                                <SE time="2:00PM" title="Panel Discussion" />
+                                <SE time="9:30AM" title="Free Time" />
+                                {/* <SE time="9:30AM" title="Pre-Service Prayer" /> */}
+                                <SE time="9:50AM" title="Pre-Service Game" />
+                                <SE time="10:00AM" title="Worship" />
+                                <SE time="10:15AM" title="Special Guest" />
+                                <SE time="11:00AM" title="Outdoot Activity" />
+                                <SE time="12:00PM" title="Lunch" />
+                                <SE time="1:00PM" title="Panel Discussion" />
                                 <SE time="4:00PM" title="Free Time" />
-                                <SE time="7:00PM" title="Dinner Party Starts" />
-                                <SE time="12:30PM" title="Lights Out" />
+                                <SE time="7:00PM" title="Dinner Party" />
+                                <SE time="12:AM" title="Lights Out" />
                             </ul>
                         </li>
                         <li>
                             <ul>
                                 <SDAY>SUN, MAR 22</SDAY>
                                 <SE time="8:30AM" title="Breakfast" />
-                                <SE time="9:30AM" title="Clean Up" />
-                                <SE time="10:00AM" title="Sermon" />
-                                <SE time="2:30PM" title="Leave Camp" />
+                                <SE time="9:30AM" title="Pack and Clean Up" />
+                                {/* <SE time="9:45AM" title="Pre-Service Prayer" /> */}
+                                <SE time="10:00AM" title="Pre-Service Game" />
+                                <SE time="10:10AM" title="Worship" />
+                                <SE time="10:30AM" title="Special Guest" />
+                                <SE time="11:40AM" title="Group Photo" />
+                                <SE time="12:00PM" title="CAMP DONE!" />
                             </ul>
                         </li>
                     </ul>
